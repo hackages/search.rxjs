f25ad82c9e49469a71dee61eeb16b8d290a6c5ff
davyengone <davy@hackages.io>
2019-10-10 08:48:16 +0200

    main entry point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Book } from '../types/book';
+import { books as mockBooks } from '../mocks/books';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'megastore';
+  title = 'Bookstore by Nalys';
+
+  // Use mock data
+  books: Book[] = mockBooks;
+
+  search() {
+    // Implement the search function
+  }
 }
---
+++ src/mocks/books.ts
@@ -0,0 +1,29 @@
+import { Book } from './../types/book';
+
+export const books: Book[] = [
+  { id: 1887, title: 'Angular up', category: 'web' },
+  { id: 2897, title: 'NativeScript in Action', category: 'mobile' },
+  { id: 8370, title: 'Using React & Redux', category: 'web' },
+  { id: 2391, title: 'Data Visualisation', category: 'data engineering' },
+  { id: 5648, title: 'Build Robot with JavaScript', category: 'iot' },
+  { id: 5670, title: 'Efficient JavaScript', category: 'web' },
+  { id: 9087, title: 'Learning React Native', category: 'mobile' },
+  {
+    id: 8007,
+    title: 'Functional Programming with JavaScript',
+    category: 'web'
+  },
+  {
+    id: 9997,
+    title: 'Building Web JavaScript Applications',
+    category: 'web'
+  },
+  { id: 1847, title: 'Introduction to IoT with Tessel', category: 'iot' },
+  { id: 1948, title: 'Introduction to C#', category: 'programming language' },
+  { id: 2048, title: 'Building complex apps with Vue', category: 'web' },
+  {
+    id: 9941,
+    title: 'Functional programming with F#',
+    category: 'programming language'
+  }
+];
